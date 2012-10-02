80babb6c45f8472d468d1fd85b21b6148597a169
jonesbr <jonesbr@localhost>
2012-10-01 20:10:33 -0400

    Initial commit of Plant Search Activity and layout.

---
+++ src/com/plantplaces/PlantSearchActivity.java
@@ -0,0 +1,15 @@
+package com.plantplaces;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PlantSearchActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		// associated the plant search layout with this activity.
+		setContentView(R.layout.activity_plantsearch);
+	}
+}
