b019dddac8c8eca32df9ebcf02bee86722a678b7
jonesbr <jonesbr@localhost>
2012-10-01 20:24:06 -0400

    Add method to return plant names.

--- src/com/plantplaces/persistence/IPlantDAO.java
+++ src/com/plantplaces/persistence/IPlantDAO.java
@@ -1,10 +1,14 @@
 package com.plantplaces.persistence;
 
+import java.util.List;
+
 /**
  * Interface for fetching plant information.
  * @author jonesbr
  *
  */
 public interface IPlantDAO {
+	
+	public List<String> getPlantNames() throws Exception;
 
 }
